02db0f8aea6f182b14951107990976594d2c5663
Nguyễn Minh Hiếu <43364170+NandoShinji@users.noreply.github.com>
2020-07-05 11:21:10 +0700

    Updates

---
+++ precache-manifest.7a4f618ba774d8c9bfd02621f0fcc2e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01a52509fef107cfaec0e7fe9e19df6",
+    "url": "/Quiz_QLDA/index.html"
+  },
+  {
+    "revision": "698df9a4be1eaa59dcb7",
+    "url": "/Quiz_QLDA/static/css/main.ced455eb.chunk.css"
+  },
+  {
+    "revision": "abe3abdf37e5d0d4a769",
+    "url": "/Quiz_QLDA/static/js/2.dde31a20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz_QLDA/static/js/2.dde31a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698df9a4be1eaa59dcb7",
+    "url": "/Quiz_QLDA/static/js/main.d21abb57.chunk.js"
+  },
+  {
+    "revision": "63d17edf20c43eb21a04",
+    "url": "/Quiz_QLDA/static/js/runtime-main.56cc82d8.js"
+  }
+]);
